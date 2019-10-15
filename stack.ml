ddec834aea9fe5abd11fe97101ded26e5d37834f
Bertonc98 <bertonc98@gmail.com>
2019-10-15 23:26:50 +0200

    stack first base

---
+++ stack.ml
@@ -0,0 +1,15 @@
+type stack =
+
+exception Empty_stack;;
+
+let push s el = el::s;;
+
+let top = function
+  [] -> raise Empty_stack;;
+  |h::t -> h;;
+
+let pop = function
+  [] -> raise Empty_stack;;
+  |h::t -> (h,t);;
+
+let empty() = [];;
