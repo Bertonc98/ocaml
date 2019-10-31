76f08652b61c03ef6cea8c2b126e8ac83e3ffd65
Bertonc98 <bertonc98@gmail.com>
2019-10-31 15:24:15 +0100

    KwIC generator completed

--- kwic.ml
+++ kwic.ml
@@ -4,6 +4,7 @@ sig
   val read_titles : in_channel -> (string * int) list
   val permute : (string * 'a) list -> ('a * string list * string list) list
   val get_sorted_permutation : unit -> (int * string list * string list) list
+  val print : (int * string list * string list) list -> unit
 end =
 struct
   open Char;;
@@ -51,18 +52,76 @@ struct
       match l with
         (s,n)::t-> permute t (List.append aux (rotation s n))
         |_->aux
-    in permute l []
+    in permute l [];;
+
+  let rev_s s =
+    let len = String.length s in
+    String.init len (fun i->s.[len-1-i]);;
+
+  let to_string l =
+    let rec to_string aux l=
+      match l with
+        h::t->to_string ((rev_s h)^" "^aux) t
+        |_->rev_s aux
+    in String.trim (to_string "" l);;
 
   let get_post a =
     match a with
-      (_,_,h::t)->h::t;;
+      (_,_,h::t)->to_string (h::t);;
+
+  let get_pre a =
+    match a with
+      (_,h::t,_)->to_string (h::t)
+      |(_,[],_)-> "";;
+
+  let get_line a =
+    match a with
+      (n,_,_)->string_of_int n;;
+
+  exception InvalidPermutation;;
 
   let compare_perm a b =
+
     match a with
-      (_,_,h::t) ->
+      (_,_,(ha::ta)) -> (match b with
+                          (_,_,(hb::tb)) -> if (get_post(a))=(get_post(b)) then 0
+                                          else if (get_post(a))>(get_post(b)) then 1
+                                          else -1)
+      |_->raise InvalidPermutation;;
 
 
-  let get_sorted_permutation() = List.sort compare_perm (permute (read_titles (input_channel())))
 
+  let get_sorted_permutation() = List.sort compare_perm (permute (read_titles (input_channel())));;
+
+  exception TooMuchLines;;
+
+  let print_line n =
+    if (String.length n)>5 then raise TooMuchLines
+    else
+    let rec print_line aux count =
+      if count >0 then print_line (" "^aux) (count-1)
+      else aux
+    in print_line n (5-(String.length n));;
+
+  let n_spaces n =
+    let rec n_spaces aux count =
+      if count>0 then n_spaces (" "^aux) (count-1)
+      else aux
+    in n_spaces "" n;;
+
+  let print_pre pre =
+    if (String.length pre)>33 then (String.sub pre 0 33)
+    else
+      (n_spaces (33-(String.length pre)))^pre;;
+
+  let print_post post =
+    if (String.length post)>40 then (String.sub post 0 40)
+    else
+      post^(n_spaces (40-(String.length post)));;
+
+  let rec print tuple=
+    match tuple with
+      h::t->print_string(print_line (get_line h)^" "^print_pre (get_pre h)^" "^print_post (get_post h)^"\n"); print t
+      |_->();;
 
 end;;
