2af1a1801245e2bb5da68cba48912c0d155ae945
Bertonc98 <bertonc98@gmail.com>
2019-11-04 08:23:21 +0100

    Print and trasposition

--- mop.ml
+++ mop.ml
@@ -3,6 +3,9 @@ module Mop:
     val matrix_equivalence : int array array -> int array array -> bool
     val matrix_copy : int array array -> int array array -> unit
     val ( + ) : int array array -> int array array -> int array array
+    val matrix_scalar_mul : int -> int array array -> int array array
+    val matrix_trasposition : int array array -> int array array
+    val print_matrix : int array array -> unit
   end=
   struct
 
@@ -46,5 +49,27 @@ module Mop:
             in
               Array.iteri (sum_row) a; c;;
 
+    let matrix_scalar_mul k m =
+        let c = make_skeleton(get_sizes m)
+        in
+          Array.iteri (fun x _ -> (Array.iteri (fun y _ -> c.(x).(y)<-(m.(x).(y)*k)) m.(x)) ) m; c;;
 
-  end;;
+    let matrix_trasposition m =
+      let m_size = get_sizes m
+      in
+      match m_size with
+        (a,b) when (a=1) && (b=1) ->m
+        |(a,b)->
+               let c = make_skeleton (b,a)
+               in
+                 let swap x y _= c.(x).(y)<-m.(y).(x)
+                 in
+                   Array.iteri (fun i _ -> (Array.iteri (swap i) c.(i))) c; c;;
+
+    let print_matrix m =
+      let print i j _ = Printf.printf "%d\t" m.(i).(j)
+      in
+        let print_row i _ = (Array.iteri (print i) m.(i)); Printf.printf "\n"
+        in
+          Array.iteri (print_row) m;;
+end;;
